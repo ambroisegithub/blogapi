33b96318136c2d64c65d3dd05483bf33a3f08c32
AMBROISE Muhayimana <107347030+ambroisegithub@users.noreply.github.com>
2023-02-10 19:33:35 +0200

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,10 @@
 const User = require("../models/user1.js");
+//const Blog = require("../routes/api")
 
 const mongoose = require("mongoose");
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = class UserController {
-
   //New User Create
   static createUser = async (req, res) => {
     let payload = req.body;
@@ -36,46 +36,45 @@ module.exports = class UserController {
     }
   };
 
-
   //Single User Information
-  static singleUser = async(req, res)=>{
+  static singleUser = async (req, res) => {
     const id = req.params.id;
 
-    try{
+    try {
+      console.log(req.params.id);
+
       const singleUserInfo = await User.findById(id);
       const { name, title, discription, avater } = singleUserInfo;
       var getImageName = avater.match(/\/([^\/?#]+)[^\/]*$/);
 
       //return console.log(getImageName);
-      
-      const singleuUserData ={
+
+      const singleuUserData = {
         name,
         title,
         discription,
-        imageUrl: `http://localhost:5000/user/${getImageName[1]}`
-      }
+        
+        imageUrl: `https://blogapi-0ew5.onrender.com/user/${getImageName[1]}`,
+      };
       return res.status(200).json({
         code: 200,
         message: "Blog Information",
         data: singleuUserData,
       });
       //return console.log(singleUserInfo)
-    }
-    catch(error){
+    } catch (error) {
       res.status(501).json({
         code: 501,
         message: error.message,
         error: true,
       });
     }
-  }
-
+  };
 
   //All User information
-  static allUser = async(req, res)=>{
-    try{
+  static allUser = async (req, res) => {
+    try {
       const allUserInfo = await User.find();
-      
 
       //return console.log(singleUserInfo);
       return res.status(200).json({
@@ -84,109 +83,73 @@ module.exports = class UserController {
         data: allUserInfo,
       });
       //return console.log(singleUserInfo)
-    }
-    catch(error){
+    } catch (error) {
       res.status(501).json({
         code: 501,
         message: error.message,
         error: true,
       });
     }
-  }
-
+  };
 
   //User Update by User Id
-  static updateUser = async(req, res)=>{
+  static updateUser = async (req, res) => {
     const id = req.params.id;
     let reqBody = req.body;
 
     //If File have then push file into reqBody then process update
-    var imgUrl = '';
-    if(req.file) var imgUrl = `storage/images/${req.file.filename}`;
+    var imgUrl = "";
+    if (req.file) var imgUrl = `storage/images/${req.file.filename}`;
     reqBody.avater = imgUrl;
-    
-    
-    try{
-        //Check user have photo/image. if had then first delete local file then database
-        const userInfo = await User.findById(id);
-        const userPhotoInfo = userInfo.avater;
-        if(userPhotoInfo){
-          fs.unlinkSync(DIR + userPhotoInfo);
-        }
 
-        const updateItem = await User.findOneAndUpdate( { _id: id }, reqBody );
-        return res.status(200).json({
-          code: 200,
-          message: "Blog Updated Successfully",
-          data: updateItem,
-        });
+    try {
+      //Check user have photo/image. if had then first delete local file then database
+      const userInfo = await User.findById(id);
+      const userPhotoInfo = userInfo.avater;
+      if (userPhotoInfo) {
+        fs.unlinkSync(DIR + userPhotoInfo);
+      }
 
-
-    }catch(error){
+      const updateItem = await User.findOneAndUpdate({ _id: id }, reqBody);
+      return res.status(200).json({
+        code: 200,
+        message: "Blog Updated Successfully",
+        data: updateItem,
+      });
+    } catch (error) {
       res.status(501).json({
         code: 501,
         message: error.message,
         error: true,
       });
     }
-
-  }
-
-
-  //User Delete By User Id
-  static deleteUser = async(req, res)=>{
-    const id = req.params.id;
-    //return console.log(id)
-    try{
-     
-        const userDeleteinfo = await User.findOneAndDelete({_id: id});
-        const {avater} = userDeleteinfo
-        
-        if(avater){
-          fs.unlinkSync(DIR + avater);
-        }
-
-        //const userDelete = await User.deleteOne({_id: id});
-        return res.status(200).json({
-          code: 200,
-          message: "Blog Deleted Successfully",
-          data: userDeleteinfo,
-        });
-    }catch(error){
-      res.status(501).json({
-        code: 501,
-        message: error.message,
-        error: true,
-      });
-    }
-  }
   };
 
-//  static commentBlogs= async(req, res) => {
-   
-//      const commented = await User.findOneAndUpdate(
-//        { _id: req.body._id },
+  //User Delete By User Id
+  static deleteUser = async (req, res) => {
+    const id = req.params.id;
+    //return console.log(id)
+    try {
+      const userDeleteinfo = await User.findOneAndDelete({ _id: id });
+      const { avater } = userDeleteinfo;
 
-//        {
-//          Comment: {
-//            name: req.body.name,
-//            title: req.body.email,
-//            discription: req.body.discription,
-//            comment: req.body.comment,
-//          },
-//        }
-//      );
-   
-//   console.log(req.body);
-//     return res.status(200).json({
-//       code: 200,
-//       message: "blog commented sucussesfully",
-//       data: commented,
-//     });
-    
-//     }
+      if (avater) {
+        fs.unlinkSync(DIR + avater);
+      }
 
+      //const userDelete = await User.deleteOne({_id: id});
+      return res.status(200).json({
+        code: 200,
+        message: "Blog Deleted Successfully",
+        data: userDeleteinfo,
+      });
+    } catch (error) {
+      res.status(501).json({
+        code: 501,
+        message: error.message,
+        error: true,
+      });
+    }
+  };
 
-
-
-
+};
