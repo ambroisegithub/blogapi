6f7d20d4fd81676e114f98549b9be5222b897359
ambroise <muhayimana21@gmail.com>
2023-02-12 18:39:54 +0200

    modified

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,114 +1,129 @@
-// const Authorization = require("../middlewares/Middlewares");
 const express = require("express");
 const UserController = require("../controllers/userController");
 const fileUpload = require("../utils/fileUpload");
 const router = express.Router();
-
 /**
  * @swagger
- * /postBlog:
- *   post:
- *     description: Create a new blog post with an optional image
- *     consumes:
- *       - multipart/form-data
- *     parameters:
- *       - in: formData
- *         name: image
- *         type: file
- *         required: false
- *         description: Image file to attach to the blog post
- *     responses:
- *       201:
- *         description: Blog post created successfully
- *       400:
- *         description: Bad Request
+ * paths:
+ *   /postBlog:
+ *     post:
+ *       tags:
+ *         - User
+ *       description: Creates a new user
+ *       parameters:
+ *         - in: formData
+ *           name: image
+ *           type: file
+ *           required: true
+ *           description: The image file to upload.
+ *       responses:
+ *         201:
+ *           description: User created successfully
+ *         400:
+ *           description: Bad Request
+ *   /api/v1/GetAll:
+ *     get:
+ *       tags:
+ *         - User
+ *       description: Get all users
+ *       responses:
+ *         200:
+ *           description: Returns all users
+ *         500:
+ *           description: Internal server error
+ *   /api/v1/singleblog/{id}:
+ *     get:
+ *       tags:
+ *         - User
+ *       description: Get a single user by id
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           type: string
+ *           required: true
+ *           description: Id of the user to retrieve
+ *       responses:
+ *         200:
+ *           description: Returns the user
+ *         404:
+ *           description: User not found
+ *         500:
+ *           description: Internal server error
+ *   /updateBlog/{id}:
+ *     post:
+ *       tags:
+ *         - User
+ *       description: Updates a user by id
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           type: string
+ *           required: true
+ *           description: Id of the user to update
+ *         - in: formData
+ *           name: image
+ *           type: file
+ *           required: true
+ *           description: The image file to upload.
+ *       responses:
+ *         200:
+ *           description: User updated successfully
+ *         404:
+ *           description: User not found
+ *         500:
+ *           description: Internal server error
+ *   /api/v1/deleteBlog/{id}:
+ *     delete:
+ *       tags:
+ *         - User
+ *       description: Deletes a user by id
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           type: string
+ *           required: true
+ *           description: Id of the user to delete
+ *       responses:
+ *         204:
+ *           description: User deleted successfully
+ *         404:
+ *           description: User not found
+ *         500:
+ *           description: Internal server error
  */
+
+
+
+
+
+
 router.post(
   "/postBlog",
+
   fileUpload("./storage/images"),
   UserController.createUser
 );
 
-/**
- * @swagger
- * /GetAll:
- *   get:
- *     description: Retrieve a list of all blog posts
- *     responses:
- *       200:
- *         description: List of blog posts
- *       404:
- *         description: No blog posts found
- */
-router.get("/GetAll",UserController.allUser);
 
-/**
- * @swagger
- * /singleblog/{id}:
- *   get:
- *     description: Retrieve a single blog post by id
- *     parameters:
- *       - in: path
- *         name: id
- *         type: string
- *         required: true
- *         description: ID of the blog post to retrieve
- *     responses:
- *       200:
- *         description: Blog post found
- *       404:
- *         description: Blog post not found
- */
+
+router.get("/GetAll", UserController.allUser);
+
+
+
 router.get("/singleblog/:id", UserController.singleUser);
 
-/**
- * @swagger
- * /updateBlog/{id}:
- *   post:
- *     description: Update an existing blog post with an optional image
- *     consumes:
- *       - multipart/form-data
- *     parameters:
- *       - in: path
- *         name: id
- *         type: string
- *         required: true
- *         description: ID of the blog post to update
- *       - in: formData
- *         name: image
- *         type: file
- *         required: false
- *         description: Image file to attach to the blog post
- *     responses:
- *       200:
- *         description: Blog post updated successfully
- *       400:
- *         description: Bad Request
- *       404:
- *         description: Blog post not found
- */
+
+
 router.post(
   "/updateBlog/:id",
+
   fileUpload("./storage/images"),
   UserController.updateUser
 );
 
-/**
- * @swagger
- * /deleteBlog/{id}:
- *   delete:
- *     description: Delete a blog post by id
- *     parameters:
- *       - in: path
- *         name: id
- *         type: string
- *         required: true
- *         description: ID of the blog post to delete
- *     responses:
- *       204:
- *         description: Blog post deleted successfully
- *       404:
- *         description: Blog post not found
- */
-router.delete("/deleteBlog/:id",UserController.deleteUser);
+
+router.delete("/deleteBlog/:id", UserController.deleteUser);
+
+// router.post("/comment/:id", UserController.commentBlogs);
+
 module.exports = router;
